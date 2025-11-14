1e2118cdc3a8f1fc7d92d9f6da9c40b0d0a0c43b
unknown <marcalmondmontellano150@gmail.com>
2025-11-15 00:42:58 +0800

    Final revision

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 from flask import Flask, render_template
-import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='Static', template_folder='templates')
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
 @app.route('/register')
 def register():
     return render_template('register.html')
 
-if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5055))
-    app.run(debug=True, port=port)
+if __name__ == "__main__":
+    app.run(debug=True, port=5055)
